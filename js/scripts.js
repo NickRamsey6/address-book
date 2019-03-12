f930b65a5db9fac6f76f9b94204364eee71c999c
Kyle Wymer and Nick Ramsey <nick@>
2019-03-11 20:46:12 -0700

    build a form to store contact info

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = []
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+//User Interface Logic
+$(document).ready(function() {
+  $("form#addresses").submit(function(event) {
+    event.preventDefault();
+    var firstName = ($("input#firstName").val());
+    var lastName = ($("input#lastName").val());
+    var phoneNumber = ($("input#phoneNumber").val());
+  });
+});
